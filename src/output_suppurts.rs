4aa6d4e687a6fea25b982c1fae8d6fc7d639bbe0
Blüchеr <rus.bondarenko2007@gmail.com>
2025-03-10 17:43:42 +0300

    Update output_suppurts.rs

--- src/output_suppurts.rs
+++ src/output_suppurts.rs
@@ -1,50 +1,39 @@
 use libatasmart::Disk;
-use libatasmart::IdentifyParsedData;
 use ansi_term::Color;
+use crate::psevdoDB::*;
 use std::fs;
-use std::io::*;
-use std::io;
-use std::string;
 
-pub fn get_disk_brend(disk: &mut Disk) -> String{
-    let model = disk.identify_parse().unwrap().model.to_uppercase();
-    if model.starts_with("WD"){
-        "Western Digital".to_string()
-    } else if model.starts_with("ST"){
-        "Seagate BarraCuda".to_string()
-    } else if model.starts_with("SB"){
-        "SmartBuy".to_string()
-    }else if model.contains("TOSHIBA"){
-        "TOSHIBA".to_string()
-    } else {
-        "Unuknow".to_string()
+pub fn get_disk_brend(disk: &mut Disk) -> &str{
+    let model = disk.identify_parse().expect("Fn: get_disk_brend: error get model").model.to_uppercase();
+    for (index, &key) in KEY_WORDS.iter().enumerate() {
+        if model.contains(key) && index < FULL_BREND.len() {
+           return FULL_BREND[index];
+        }
     }
-        
+    FULL_BREND[FULL_BREND.len() - 1]
 }
 
-pub fn get_disk_ascii(disk: &mut Disk) -> String{
-    let model = disk.identify_parse().unwrap().model.to_uppercase();
-    if model.contains("WDC"){
-        "assets/ascii_westen.txt".to_string()
-    } else if model.starts_with("ST"){
-        "assets/ascii_seageate.txt".to_string()
-    } else {
-        "assets/ascii_default.txt".to_string()
+pub fn get_disk_ascii(disk: &mut Disk) -> &str{
+    let model = disk.identify_parse().expect("Fn: get_disk_ascii: error get model").model.to_uppercase();
+    for (index, &key) in KEY_WORDS.iter().enumerate() {
+        if model.contains(key) && index < ASCII_PATH.len() {
+            return ASCII_PATH[index];
+        }
     }
+    ASCII_PATH[ASCII_PATH.len() - 1]
 }
 
 pub fn get_disk_logo_color(disk: &mut Disk) -> Color{
-    let model = disk.identify_parse().unwrap().model.to_uppercase();
-    if model.starts_with("WD"){
-        Color::Blue
-    } else if model.starts_with("ST"){
-        Color::Green
-    } else {
-        Color::Yellow
+    let model = disk.identify_parse().expect("Fn: get_disk_logo_color: error get model").model.to_uppercase();
+    for (index, &key) in KEY_WORDS.iter().enumerate() {
+        if model.contains(key) && index < LOGO_COLOR.len(){
+            return LOGO_COLOR[index];
+        }
     }
+    LOGO_COLOR[LOGO_COLOR.len() - 1]
 }
 
-pub fn get_disk_condition(disk: &mut Disk) -> usize{
+pub fn get_disk_condition(input: &str, disk: &mut Disk) -> usize{
     let bed: usize = disk.get_bad_sectors().unwrap().try_into().unwrap();
     let disk_name = disk.disk.as_os_str().to_str().unwrap()[5..8].to_string();
     let path_to_sector = format!("/sys/block/{}/queue/hw_sector_size", disk_name);
