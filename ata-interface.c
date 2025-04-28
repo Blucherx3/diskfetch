033d943ded64b93b7982bcb1fd49b1d2f3e6166d
Blucherx3 <rus.bondarenko2007@gmail.com>
2025-04-28 22:43:55 +0300

    fix instaler

--- ata-interface.c
+++ ata-interface.c
@@ -2,6 +2,7 @@
 #include <atasmart.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
 
@@ -17,29 +18,32 @@ struct disk_info_page get_sata_info_page(char* path)
     
     sk_disk_open(path, &disk);
     
-    uint64_t size;
-    sk_disk_get_size(disk, &size);
-    resault.size = size;
+    uint64_t *size = malloc(sizeof(uint64_t));
+    sk_disk_get_size(disk, size);
+    resault.size = *(long long unsigned int*)size;
+    free(size);
 
-    uint64_t temperature;
-    sk_disk_smart_get_temperature(disk, &temperature);
-    resault.temp = temperature;
+    uint64_t *temperature = malloc(sizeof(uint64_t));
+    sk_disk_smart_get_temperature(disk, temperature);
+    printf("%lu", *temperature);
+    resault.temp = (int)*(long long unsigned int*)temperature;
+    free(temperature);
 
     uint64_t count_bad_sectors;
     sk_disk_smart_get_bad(disk, &count_bad_sectors);
-    int sector_size;
+    long long unsigned int sector_size;
     char d_path[4] = {path[5], path[6], path[7], '\0'};
     char path_sector_size[64];
     snprintf(path_sector_size, 64,
         "/sys/block/%s/queue/logical_block_size", d_path);
     FILE* ff = fopen(path_sector_size, "r");
-    fscanf(ff, "%d", &sector_size);
+    fscanf(ff, "%llu", &sector_size);
     fclose(ff);
-    resault.health = (count_bad_sectors/(size/sector_size)) * 100;
+    resault.health = ((int)(count_bad_sectors/(resault.size/sector_size)) * 100);
 
     uint64_t power_c;
     sk_disk_smart_get_power_cycle(disk, &power_c);
-    resault.power_cycles = power_c;
+    resault.power_cycles = *(__u64*)&power_c;
 
     uint64_t power_h;
     sk_disk_smart_get_power_on(disk, &power_h);
--- install.sh
+++ install.sh
@@ -6,3 +6,19 @@ LIBS="nvme atasmart"
 
 
 gcc -g3 $FILES -o diskfetch $(echo "$LIBS" | sed 's/ / -l/g' | sed 's/^/-l/') 
+
+
+mkdir /usr/local/bin/diskfetch 
+mv $(pwd)/diskfetch /usr/local/bin/diskfetch/ 
+
+cp -r $(pwd)/assets $HOME/.local/bin/diskfetch/
+
+touch $HOME/.local/bin/diskfetch/diskfetch.sh
+
+cd $HOME/.local/bin/diskfetch
+echo "#!/bin/bash" > diskfetch.sh
+echo "sudo /usr/local/bin/diskfetch/diskfetch \"\$@\"" >> diskfetch.sh
+
+chmod +x diskfetch.sh
+
+sudo ln -sf diskfetch.sh /usr/bin/diskfetch
